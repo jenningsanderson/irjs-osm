31a2f7af59315e1697e019824bcfca14c8cac024
rsoden <robert.soden@gmail.com>
2015-02-07 19:58:12 -0800

    playing with event emitters

--- example.js
+++ example.js
@@ -1,9 +1,8 @@
 var fs  = require('fs')
+var osmFeed = require('./lib/osm')
 
 var bbox = fs.readFileSync('./bbox.config','utf8')
-url = 'http://zverik.osm.rambler.ru/whodidit/scripts/rss.php?bbox=' + bbox
+var url = 'http://zverik.osm.rambler.ru/whodidit/scripts/rss.php?bbox=' + bbox
+var num_results = 10
 
-var osmFeed = require('./lib/test')
-var feed = osmFeed(url)
-
-feed.titles()
+osmFeed.titles(url,num_results)
