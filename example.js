74c1d14b87e3b2423aeef338df5383ca5b8c1f2f
rsoden <robert.soden@gmail.com>
2015-02-07 20:17:13 -0800

    adding in command line stuff

--- example.js
+++ example.js
@@ -1,8 +1,4 @@
-var fs  = require('fs')
 var osmFeed = require('./lib/osm')
-
-var bbox = fs.readFileSync('./bbox.config','utf8')
-var url = 'http://zverik.osm.rambler.ru/whodidit/scripts/rss.php?bbox=' + bbox
 var num_results = 10
 
-osmFeed.titles(url,num_results)
+osmFeed.titles(num_results)
