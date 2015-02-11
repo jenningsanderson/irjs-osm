22f41bc08be8a8ee040fde8ba06d84b4228bbf4d
Jennings Anderson <jennings.anderson@colorado.edu>
2015-02-11 13:12:53 -0700

    updated the tests to not timeout

--- test/titles.test.js
+++ test/titles.test.js
@@ -4,9 +4,9 @@ var assert = require("assert")
 var osmFeed = require('../lib')
 
 describe('Titles', function() {
-
+	this.timeout(15000);
     it('should grab 10 Titles', function(done) {
-        
+
         osmFeed.get({limit:10,outFile:'return'}, osmFeed.titles, function(res){
             assert.equal(res.split('\n').length, 10)
             done()
--- test/users.test.js
+++ test/users.test.js
@@ -4,9 +4,9 @@ var assert = require("assert")
 var osmFeed = require('../lib')
 
 describe('Users', function() {
-
+	this.timeout(15000);
     it('should grab 3 users', function(done) {
-        
+
         osmFeed.get({limit:3,outFile:'return'}, osmFeed.users, function(res){
             assert.equal(res.split('\n').length, 3)
             done()
