6b100172f0979e7c332ff24388007072bac1c973
rsoden <robert.soden@gmail.com>
2015-02-17 16:57:38 -0700

    geojson

--- example.js
+++ example.js
@@ -2,7 +2,8 @@ var osmFeed = require('./lib')
 
 options = {}
 options.limit = 3
-options.outFile = 'db'
+options.format = 'geojson'
+options.outFile = './example.geojson'
 
 //You may pass a bbox in as a string, or as a location by using either of the following (not both).
 
@@ -12,21 +13,7 @@ options.bboxFile = './config.bbox'
 //OR Pass a String
 options.bbox = '-112.076415,46.572087,-111.982267,46.629261'
 
-var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-	host: 'localhost:9200',
-	log: 'trace'
-});
-
-client.search({
-	q: 'pants'
-}).then(function (body) {
-	var hits = body.hits.hits;
-}, function (error) {
-	console.trace(error.message);
-});
-
 //Print out the changesets
 osmFeed.get(options, osmFeed.geo, function(res){
-	console.log(res)
+//	console.log(res)
 })
