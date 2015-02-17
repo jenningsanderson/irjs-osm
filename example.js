0c4eda55dfdaa050985cbe6409f17e206943ab8c
Jennings Anderson <jennings.anderson@colorado.edu>
2015-02-16 23:43:43 -0700

    changed eaxmple

--- example.js
+++ example.js
@@ -12,6 +12,20 @@ options.bboxFile = './config.bbox'
 //OR Pass a String
 options.bbox = '-112.076415,46.572087,-111.982267,46.629261'
 
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+	host: 'localhost:9200',
+	log: 'trace'
+});
+
+client.search({
+	q: 'pants'
+}).then(function (body) {
+	var hits = body.hits.hits;
+}, function (error) {
+	console.trace(error.message);
+});
+
 //Print out the changesets
 osmFeed.get(options, osmFeed.changesets, function(res){
 	console.log(res)
