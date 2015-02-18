ec7c880fd61740feae5bf34fa3c2266cd0b44dbf
rsoden <robert.soden@gmail.com>
2015-02-18 12:41:27 -0700

    geo

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 var osmFeed = require('./lib')
 
 options = {}
-options.limit = 3
+options.limit = 20
 options.format = 'geojson'
 options.outFile = 'example.geojson'
 
@@ -11,7 +11,7 @@ options.outFile = 'example.geojson'
 options.bboxFile = './config.bbox'
 
 //OR Pass a String
-options.bbox = '-112.076415,46.572087,-111.982267,46.629261'
+// options.bbox = '-112.076415,46.572087,-111.982267,46.629261'
 
 //Print out the changesets
 osmFeed.get(options, osmFeed.geo, function(res){
